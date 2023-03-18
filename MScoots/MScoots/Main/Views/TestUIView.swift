60c3d39b1c06365129d17433d313001abc185e64
Justin Trubela <jtrubela@icloud.com>
2023-03-18 00:38:00 -0400

    LoadingView .... Shade wants to do an introductory loading screen before accessing place to sign in or register....video on how to do it...also noticed some object in file named Breakpoints_v2.xcbkptlist and want to understand at some point what that represents

---
+++ MScoots/MScoots/Main/Views/TestUIView.swift
@@ -0,0 +1,20 @@
+//
+//  TestUIView.swift
+//  MScoots
+//
+//  Created by Justin Trubela on 3/17/23.
+//
+
+import SwiftUI
+
+struct TestUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestUIView()
+    }
+}
