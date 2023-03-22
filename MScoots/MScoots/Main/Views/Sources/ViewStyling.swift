0566a71a477d2acbd8a04b3ced5738c2715e7f25
Justin Trubela <jtrubela@icloud.com>
2023-03-21 20:39:15 -0400

    Establishing an organized custom view item/object modifier class to keep reciprocity and convention throughout similar aspects of the project. Also implemented an instance of a code snippet for .onAppear struct that handles navigation views ultimately making transitions between views an inheritable/re-usable bit of code

--- MScoots/MScoots/Main/Views/Sources/ViewStyling.swift
+++ MScoots/MScoots/Main/Views/Sources/ViewStyling.swift
@@ -4,10 +4,197 @@
 //
 //  Created by Justin Trubela on 2/27/23.
 //
+//
+//  The purpose for this file is to create a standardized way to add custom modifiers to achieve a uniform
+//      output for items used throughout the projects
+//
+//
+//  For modifying elements for view content items you need to establish the following:
+//      an extension of View that returns some view
+//      a struct that uses that returned view
+//
 
 import Foundation
 import SwiftUI
 
+
+/*-----------------------------------------------------------------------------------------------------/
+ //Views/Scenes                Views/Scenes                 Views/Scenes                     Views/Scenes
+ //                     Views/Scenes                                             Views/Scenes
+ //     Views/Scenes        Views/Scenes      Views/Scenes         Views/Scenes                 Views/Scenes
+ //             Views/Scenes               Views/Scenes                        Views/Scenes
+ //    Views/Scenes         Views/Scenes         Views/Scenes         Views/Scenes         Views/Scenes
+ /------------------------------------------------------------------------------------------------------*/
+
+
+struct CustomScene: Scene {
+    
+    struct ViewItem: Identifiable, Hashable {
+        let name: String
+        let id = NSUUID().uuidString
+    }
+    
+    let views: [ViewItem] = [
+        //Login/Register
+        .init(name: "LandingPageView"),
+        .init(name: "RegistrationView"),
+        .init(name: "LoginView"),
+        .init(name: "ResetPasswordView"),
+        
+        .init(name: "UserHomeView"),
+        .init(name: "ScanQRView"),
+        .init(name: "ScooterListView"),
+        .init(name: "ScootMapView"),
+        .init(name: "MyScootsView"),
+        .init(name: "UserProfile_SettingsView"),
+        .init(name: "WalletView"),
+        //Admin.....
+        .init(name: "AdminUserHomeView"),
+        .init(name: "AdminScooterManagerView"),
+        .init(name: "AdminUserManagerView"),
+        .init(name: "AdminFindScooterView")
+            
+    ]
+    @Environment(\.scenePhase) private var scenePhase
+    @State private var navigationPath = [ViewItem]()
+
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack(path: $navigationPath){
+                ZStack{
+                    NavigationLink("LoginView", destination: ViewForItem(ViewItem(name: "LoginView")))
+
+                }.navigationDestination(for: ViewItem.self){ view in
+                    VStack{
+                        ViewForItem(view)
+                    }
+                }.navigationBarBackButtonHidden(true)
+            }
+        }
+        .onChange(of: scenePhase) { newScenePhase in
+            if newScenePhase != .active {
+                DispatchQueue.main.async {
+
+                }
+            }
+        }
+    }
+    //update the view to the navigationstack path variable
+    func ViewForItem(_ view: ViewItem) -> AnyView {
+        switch view.name {
+        case "LandingPageView":
+            return AnyView(LandingPageView())
+        case "LoginView":
+            return AnyView(LoginView())
+        case "RegistrationView":
+            return AnyView(RegistrationView())
+        case "UserHomeView":
+                return AnyView(UserHomeView())
+        case "ResetPasswordView":
+            return AnyView(ResetPasswordView())
+        case "AdminUserHomeView":
+            return AnyView(AdminUserHomeView())
+        case "ScanQRView":
+            return AnyView(ScanQRView())
+        case "ScooterListView":
+            return AnyView(ScooterListView())
+        case "ScootMapView":
+            return AnyView(ScootMapView())
+        case "MyScootsView":
+            return AnyView(MyScootsView())
+        case "UserProfile_SettingsView":
+            return AnyView(UserProfile_SettingsView())
+        case "WalletView":
+            return AnyView(WalletView())
+        default:
+            return AnyView(LandingPageView())
+        }
+    }
+}
+
+/*-----------------------------------------------------------------------------------------------------/
+ //Backgrounds                Backgrounds                 Backgrounds                     Backgrounds
+ //                     Backgrounds                                             Backgrounds
+ //     Backgrounds        Backgrounds      Backgrounds         Backgrounds                 Backgrounds
+ //             Backgrounds               Backgrounds                        Backgrounds
+ //    Backgrounds         Backgrounds         Backgrounds         Backgrounds         Backgrounds
+ /------------------------------------------------------------------------------------------------------*/
+
+
+extension View {
+    func MakeRoundedSquareBackground() -> some View {
+        modifier(SquareBackground())
+    }
+}
+
+
+struct LoginView_Background_Style: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundStyle(.linearGradient(colors: [.pink, .red], startPoint: .topLeading, endPoint: .bottomTrailing))
+            .frame(width: 1000, height: 300)
+            .rotationEffect(.degrees(135))
+            .offset(y: -440)
+    }
+}
+extension View {
+    func MakeLoginView_Background_Style() -> some View {
+        modifier(LoginView_Background_Style())
+    }
+}
+
+
+struct mySquareBackground: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 350, height: 500)
+            .background(.thickMaterial)
+//            .background(Color.secondary)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding(5)
+    }
+}
+extension View {
+    func MakeMyBackground() -> some View {
+        modifier(mySquareBackground())
+    }
+}
+
+struct SquareBackground: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: UIScreen.main.bounds.width-50, maxHeight: 200)
+            .background(.thinMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .padding(5)
+    }
+}
+
+
+/*------------------------------------------------------------------------------------------------------/
+ //Images                              Images                 Images                     Images
+ //                     Images                                             Images
+ //     Images            Images              Images         Images                 Images
+ //             Images               Images                                Images
+ //    Images         Images         Images         Images         Images                  Images
+ /------------------------------------------------------------------------------------------------------*/
+
+struct myHamburgerStyler: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 20))
+            .scaleEffect(x: 2, y: 3)
+            .padding(.trailing,30)
+            .foregroundColor(Color.red)
+            .bold()
+            .shadow(color: .black, radius: 5, x: 6, y: 8)
+    }
+}
+extension View {
+    func addMyHambugerMenuStyler() -> some View {
+        modifier (myHamburgerStyler())
+    }
+}
 
 struct LEDMaker: ViewModifier {
     func body(content: Content) -> some View {
@@ -34,113 +221,47 @@ extension View {
 }
 
 
-struct SquareBackground: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .frame(maxWidth: UIScreen.main.bounds.width-50, maxHeight: 200)
-            .background(.thinMaterial)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
-            .padding(5)
-    }
-}
-extension View {
-    func MakeRoundedSquareBackground() -> some View {
-        modifier(SquareBackground())
+struct FlagImage: View {
+    var ImageText: String
+    
+    var body: some View {
+        Image(ImageText)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .shadow(radius: 50)
     }
 }
 
-
-struct LoginView_Background_Style: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .foregroundStyle(.linearGradient(colors: [.pink, .red], startPoint: .topLeading, endPoint: .bottomTrailing))
-            .frame(width: 1000, height: 300)
-            .rotationEffect(.degrees(135))
-            .offset(y: -440)
-    }
-}
-extension View {
-    func MakeLoginView_Background_Style() -> some View {
-        modifier(LoginView_Background_Style())
-    }
-}
-
-struct TextEntryField: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .padding(20)
-            .frame(width: 350, height: 50, alignment: .center)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
-            .font(.title2)
-            .textFieldStyle(.plain)
-            .padding(10)
-    }
-}
-extension View {
-    func MakeTextFieldEntry() -> some View {
-        modifier(TextEntryField())
-    }
-}
-
-struct ButtonStyle: ViewModifier {
+/*------------------------------------------------------------------------------------------------------/
+ //Buttons                              Buttons                 Buttons                     Buttons
+ //                     Buttons                                             Buttons
+ //     Buttons            Buttons              Buttons         Buttons                 Buttons
+ //             Buttons               Buttons                                Buttons
+ //    Buttons         Buttons         Buttons         Buttons         Buttons                  Buttons
+ /------------------------------------------------------------------------------------------------------*/
+struct LoginView_Login_ButtonStyle: ViewModifier {
     func body(content: Content) -> some View {
         content
         
             .frame(width: 150, height: 50)
-            .background(Color.white)
-            .background(RoundedRectangle(cornerRadius: 10,style:.continuous))
+            .background(
+                RoundedRectangle(cornerRadius: 10,style:.continuous)
+                    .fill(.white)
+            )
             .clipShape(Capsule())
         
-            .font(.title2).fontWeight(.bold)
+            .fontWeight(.bold)
             .foregroundColor(.red)
             .padding(20)
     }
 }
 extension View {
-    func Add_ButtonSytle() -> some View {
-        modifier(ButtonStyle())
+    func Add_LoginView_ButtonStyle() -> some View {
+        modifier(LoginView_Login_ButtonStyle())
     }
 }
 
-
-
-struct myDivider: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .frame(width: 380, height: 5)
-            .foregroundColor(.white)
-            .padding(15)
-        
-    }
-}
-extension View {
-    func AddMyDivider() -> some View {
-        modifier(myDivider())
-    }
-}
-
-
-
-struct RegisterAccountButton: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .frame(width: 300,height: 50)
-            .background(Color.white)
-            .cornerRadius(10)
-            .clipShape(Capsule())
-            .padding(15)
-        
-    }
-}
-extension View {
-    func AddRegisterAccountButton() -> some View {
-        modifier (RegisterAccountButton())
-    }
-}
-
-
-struct myNavigationViewStyler2: ViewModifier{
+struct AddMyButtonStyle: ViewModifier {
     func body(content: Content) -> some View {
         content
             .frame(width: 180, height: 50)
@@ -152,11 +273,10 @@ struct myNavigationViewStyler2: ViewModifier{
     }
 }
 extension View {
-    func AddMyNavigationViewStyler2() -> some View {
-        modifier (myNavigationViewStyler2())
+    func AddMy_ButtonSytle() -> some View {
+        modifier(AddMyButtonStyle())
     }
 }
-
 
 struct addMyForEach_HomeViewButtonStyler2: ViewModifier{
     func body(content: Content) -> some View {
@@ -189,113 +309,138 @@ extension View {
     }
 }
 
+struct RegisterAccountButton: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 300,height: 50)
+            .background(Color.white)
+            .cornerRadius(10)
+            .clipShape(Capsule())
+            .padding(15)
+        
+    }
+}
+extension View {
+    func AddRegisterAccountButton() -> some View {
+        modifier (RegisterAccountButton())
+    }
+}
 
-
-struct LoginView_Login_ButtonStyle: ViewModifier {
+struct ButtonStyle: ViewModifier {
     func body(content: Content) -> some View {
         content
         
             .frame(width: 150, height: 50)
-            .background(
-                RoundedRectangle(cornerRadius: 10,style:.continuous)
-                    .fill(.white)
-            )
-            .clipShape(Capsule())
-        
-            .fontWeight(.bold)
-            .foregroundColor(.red)
-            .padding(20)
-    }
-}
-extension View {
-    func Add_LoginView_ButtonStyle() -> some View {
-        modifier(LoginView_Login_ButtonStyle())
-    }
-}
-
-struct myHamburgerStyler: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.system(size: 20))
-            .scaleEffect(x: 2, y: 3)
-            .padding(.trailing,30)
-            .foregroundColor(Color.red)
-            .bold()
-            .shadow(color: .black, radius: 5, x: 6, y: 8)
-    }
-}
-extension View {
-    func addMyHambugerMenuStyler() -> some View {
-        modifier (myHamburgerStyler())
-    }
-}
-
-
-struct AddMyButtonStyle: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .frame(width: 180, height: 50)
-            .background(Color.black)
+            .background(Color.white)
             .background(RoundedRectangle(cornerRadius: 10,style:.continuous))
             .clipShape(Capsule())
-            .font(.title3).fontWeight(.bold)
+        
+            .font(.title2).fontWeight(.bold)
             .foregroundColor(.red)
+            .padding(20)
     }
 }
 extension View {
-    func AddMy_ButtonSytle() -> some View {
-        modifier(AddMyButtonStyle())
+    func Add_ButtonSytle() -> some View {
+        modifier(ButtonStyle())
     }
 }
 
 
-struct mySquareBackground: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .frame(width: 350, height: 500)
-            .background(.thickMaterial)
-//            .background(Color.secondary)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .padding(5)
-    }
-}
-extension View {
-    func MakeMyBackground() -> some View {
-        modifier(mySquareBackground())
-    }
-}
+ /*-----------------------------------------------------------------------------------------------------/
+  //TextObjects                TextObjects                 TextObjects                     TextObjects
+  //                     TextObjects                                             TextObjects
+  //     TextObjects        TextObjects      TextObjects         TextObjects                 TextObjects
+  //             TextObjects               TextObjects                        TextObjects
+  //    TextObjects         TextObjects         TextObjects         TextObjects         TextObjects
+  /------------------------------------------------------------------------------------------------------*/
 
+  struct MyTextEntryField: ViewModifier {
+      func body(content: Content) -> some View {
+          content
+              .padding(20)
+              .frame(width: 320, height: 50, alignment: .center)
+              .border(.black)
+              .shadow(color: .red,radius: 30)
+              .background(Color.white)
+              .clipShape(RoundedRectangle(cornerRadius: 0))
+              .font(.title2)
+              .textFieldStyle(.plain)
+              .padding(10)
+      }
+  }
+  extension View {
+      func AddMyTextFieldEntry() -> some View {
+          modifier(MyTextEntryField())
+      }
+  }
 
-struct MyTextEntryField: ViewModifier {
+struct TextEntryField: ViewModifier {
     func body(content: Content) -> some View {
         content
             .padding(20)
-            .frame(width: 320, height: 50, alignment: .center)
-            .border(.black)
-            .shadow(color: .red,radius: 30)
+            .frame(width: 350, height: 50, alignment: .center)
             .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 0))
+            .clipShape(RoundedRectangle(cornerRadius: 20))
             .font(.title2)
             .textFieldStyle(.plain)
             .padding(10)
     }
 }
 extension View {
-    func AddMyTextFieldEntry() -> some View {
-        modifier(MyTextEntryField())
+    func MakeTextFieldEntry() -> some View {
+        modifier(TextEntryField())
     }
 }
+  
+  
+  
+  
+  /*------------------------------------------------------------------------------------------------------/
+ //Objects                              Objects                 Objects                     Objects
+ //                     Objects                                             Objects
+ //     Objects            Objects              Objects         Objects                 Objects
+ //             Objects               Objects                                Objects
+ //    Objects         Objects         Objects         Objects         Objects                  Objects
+ /------------------------------------------------------------------------------------------------------*/
+
+
+
+struct myDivider: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 380, height: 5)
+            .foregroundColor(.white)
+            .padding(15)
+        
+    }
+}
+extension View {
+    func AddMyDivider() -> some View {
+        modifier(myDivider())
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 /*-----------------------------END>---Custom Styling---<END--------------------------*/
 
 
-struct LandingPageView_Previews: PreviewProvider {
-    static var previews: some View {
-        LandingPageView()
-    }
-}
+
 
 
 
